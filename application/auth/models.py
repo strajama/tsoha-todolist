1ed649e4eb13d7e4878de3aee8cd0ea9f8c5ffb1
Susanna Rajamäki <strajama@gmail.com>
2019-01-31 19:29:28 +0200

    ensimmäinen yhteenvetokysely

--- application/auth/models.py
+++ application/auth/models.py
@@ -1,6 +1,8 @@
 from application import db
 from application.models import Base
 
+from sqlalchemy.sql import text
+
 class User(Base):
 
     __tablename__ = "account"
@@ -27,3 +29,18 @@ class User(Base):
 
     def is_authenticated(self):
         return True
+
+    @staticmethod
+    def find_users_with_no_tasks():
+        stmt = text("SELECT Account.id, Account.name FROM Account"
+                     " LEFT JOIN Task ON Task.account_id = Account.id"
+                     " WHERE (Task.done IS null OR Task.done = 1)"
+                     " GROUP BY Account.id"
+                     " HAVING COUNT(Task.id) = 0")
+        res = db.engine.execute(stmt)
+
+        response = []
+        for row in res:
+            response.append({"id":row[0], "name":row[1]})
+
+        return response
