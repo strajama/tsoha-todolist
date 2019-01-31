3e1b7390f1937c550ec0d0eee265913d7021038d
Susanna Rajamäki <strajama@gmail.com>
2019-01-31 20:46:25 +0200

    autorisointivalmistelua

--- application/__init__.py
+++ application/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 app = Flask(__name__)
 
+# database connectivity and ORM
 from flask_sqlalchemy import SQLAlchemy
 
 import os
@@ -14,6 +15,50 @@ else:
   
 db = SQLAlchemy(app)
 
+
+# login functionality
+from application.auth.models import User
+from os import urandom
+app.config["SECRET_KEY"] = urandom(32)
+
+from flask_login import LoginManager
+login_manager = LoginManager()
+login_manager.setup_app(app)
+
+login_manager.login_view = "auth_login"
+login_manager.login_message = "Please login to use this functionality."
+
+# roles in login_required
+from functools import wraps
+
+def login_required(role="ANY"):
+    def wrapper(fn):
+        @wraps(fn)
+        def decorated_view(*args, **kwargs):
+            if not current_user:
+                return login_manager.unauthorized()
+
+            if not current_user.is_authenticated():
+                return login_manager.unauthorized()
+            
+            unauthorized = False
+
+            if role != "ANY":
+                unauthorized = True
+                
+                for user_role in current_user.roles():
+                    if user_role == role:
+                        unauthorized = False
+                        break
+
+            if unauthorized:
+                return login_manager.unauthorized()
+            
+            return fn(*args, **kwargs)
+        return decorated_view
+    return wrapper
+
+# load application content
 from application import views
 
 from application.tasks import models
@@ -22,18 +67,8 @@ from application.tasks import views
 from application.auth import models
 from application.auth import views
 
-
-# login
+# login functionality, part 2
 from application.auth.models import User
-from os import urandom
-app.config["SECRET_KEY"] = urandom(32)
-
-from flask_login import LoginManager
-login_manager = LoginManager()
-login_manager.init_app(app)
-
-login_manager.login_view = "auth_login"
-login_manager.login_message = "Please login to use this functionality."
 
 @login_manager.user_loader
 def load_user(user_id):
