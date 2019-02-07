ab2400df73547baf6b8b615d0247e1d92a92b21a
Susanna Rajamäki <strajama@gmail.com>
2019-02-07 22:42:20 +0200

    lisää bootstrappia

--- application/auth/views.py
+++ application/auth/views.py
@@ -11,8 +11,6 @@ def auth_login():
         return render_template("auth/loginform.html", form = LoginForm())
 
     form = LoginForm(request.form)
-    if not form.validate():
-        return render_template("auth/loginform.html", form = form)
 
     user = User.query.filter_by(username=form.username.data, password=form.password.data).first()
     if not user:
