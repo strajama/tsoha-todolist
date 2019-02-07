3be35ad9fda9de4b58288ccacbf2496f86145870
Susanna Rajamäki <strajama@gmail.com>
2019-02-07 23:47:52 +0200

    korjattu inner join kysely sisennykset ja tulos näkyy etusivulla

--- application/tags/models.py
+++ application/tags/models.py
@@ -11,16 +11,16 @@ class Tag(Base):
     def __init__(self, name):
         self.name = name
 
-@staticmethod
-def find_tags_that_assigned():
-    stmt=text("SELECT Tag.id AS id, Tag.name as name FROM Tag "
-                "INNER JOIN Tags ON Tag.id=Tags.tag_id "
-                "INNER JOIN Task ON Tags.task_id=Task.id;")
+    @staticmethod
+    def find_tags_that_assigned_task():
+        stmt=text("SELECT Tag.id AS id, Tag.name as name FROM Tag "
+                    "INNER JOIN Tags ON Tag.id=Tags.tag_id "
+                    "INNER JOIN Task ON Tags.task_id=Task.id;")
 
-    res = db.engine.execute(stmt)
+        res = db.engine.execute(stmt)
 
-    response = []
-    for row in res:
-        response.append({"id":row[0], "name":row[1]})
+        response = []
+        for row in res:
+            response.append({"id":row[0], "name":row[1]})
 
-    return response
+        return response
--- application/views.py
+++ application/views.py
@@ -1,7 +1,11 @@
 from flask import render_template
 from application import app
 from application.auth.models import User
+from application.tags.models import Tag
 
 @app.route("/")
 def index():
-    return render_template("index.html", needs_tasks=User.find_users_with_no_tasks(), unfinished_tasks=User.find_users_with_unfinished_tasks())
+    return render_template("index.html", 
+            eeds_tasks=User.find_users_with_no_tasks(), 
+            unfinished_tasks=User.find_users_with_unfinished_tasks(),
+            used_tags=Tag.find_tags_that_assigned_task())
