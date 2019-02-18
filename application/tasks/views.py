b436f02cb68da3aeabca7c35a40f0dbb595c7fb0
Susanna Rajamäki <strajama@gmail.com>
2019-02-18 14:34:00 +0200

    korjattu login-muutokset toimiviksi

--- application/tasks/views.py
+++ application/tasks/views.py
@@ -1,7 +1,7 @@
 from flask import render_template, request, redirect, url_for
 from flask_login import login_required, current_user
 
-from application import app, db
+from application import app, db, login_required
 from application.tasks.models import Task
 from application.tasks.forms import TaskForm
 
@@ -12,13 +12,12 @@ def tasks_index():
     return render_template("tasks/list.html", tasks = Task.query.all())
 
 @app.route("/tasks/new/", methods=["GET"])
-@login_required
+@login_required(role="ADMIN")
 def tasks_form():
     return render_template("tasks/new.html", form = TaskForm())
 
-@login_required(role="ADMIN")
 @app.route("/tasks/", methods=["POST"])
-@login_required
+@login_required(role="ADMIN")
 def tasks_create():
     form = TaskForm(request.form)
 
@@ -36,12 +35,12 @@ def tasks_create():
     return redirect(url_for("tasks_index"))
 
 @app.route("/tasks/edit/<task_id>/", methods=["GET"])
-@login_required
+@login_required(role="ADMIN")
 def tasks_edit(task_id):
     return render_template("tasks/edit.html", task_id = task_id, form = TaskForm(), task=Task.query.get(task_id))
 
 @app.route("/tasks/edit/<task_id>/", methods=["POST"])
-@login_required
+@login_required(role="ADMIN")
 def tasks_editor(task_id):
     form = TaskForm(request.form)
     task = Task.query.get(task_id)
@@ -66,7 +65,7 @@ def tasks_editor(task_id):
     return redirect(url_for("tasks_edit", task_id = task_id))
   
 @app.route("/tasks/<task_id>/", methods=["POST"])
-@login_required
+@login_required(role="ADMIN")
 def tasks_delete(task_id):
     task = Task.query.get(task_id)
 
