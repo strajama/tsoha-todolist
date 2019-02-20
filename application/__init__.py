f73d20f164d1496164b85cd62ec71dd064f8398f
Susanna Rajamäki <strajama@gmail.com>
2019-02-20 20:06:31 +0200

    palautettu taskeihin perus login_required

--- application/__init__.py
+++ application/__init__.py
@@ -12,53 +12,8 @@ else:
     app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///tasks.db"    
     app.config["SQLALCHEMY_ECHO"] = True
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-  
+ 
 db = SQLAlchemy(app)
-
-
-# login functionality
-# from application.auth.models import User
-from os import urandom
-app.config["SECRET_KEY"] = urandom(32)
-
-from flask_login import LoginManager, current_user
-login_manager = LoginManager()
-login_manager.setup_app(app)
-
-login_manager.login_view = "auth_login"
-login_manager.login_message = "Please login to use this functionality."
-
-# roles in login_required
-# roles in login_required
-from functools import wraps
-
-def login_required(role="ANY"):
-    def wrapper(fn):
-        @wraps(fn)
-        def decorated_view(*args, **kwargs):
-            if not current_user:
-                return login_manager.unauthorized()
-          
-            if not current_user.is_authenticated():
-                return login_manager.unauthorized()
-            
-            unauthorized = False
-
-            if role != "ANY":
-                unauthorized = True
-                
-                for user_role in current_user.roles():
-                    if user_role == role:
-                        unauthorized = False
-                        break
-
-            if unauthorized:
-                return login_manager.unauthorized()
-            
-            return fn(*args, **kwargs)
-        return decorated_view
-    return wrapper
 
 # load application content
 from application import views
@@ -72,8 +27,17 @@ from application.auth import views
 from application.tags import models
 from application.tags import views
 
-# login functionality, part 2
+# login functionality
 from application.auth.models import User
+from os import urandom
+app.config["SECRET_KEY"] = urandom(32)
+
+from flask_login import LoginManager
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+login_manager.login_view = "auth_login"
+login_manager.login_message = "Please login to use this functionality."
 
 @login_manager.user_loader
 def load_user(user_id):
--- application/tasks/views.py
+++ application/tasks/views.py
@@ -1,7 +1,7 @@
 from flask import render_template, request, redirect, url_for
-from flask_login import current_user
+from flask_login import current_user, login_required
 
-from application import app, db, login_required
+from application import app, db
 from application.tasks.models import Task
 from application.tasks.forms import TaskForm
 from application.tags.models import Tag
@@ -11,12 +11,12 @@ def tasks_index():
     return render_template("tasks/list.html", tasks = Task.query.all())
 
 @app.route("/tasks/new/", methods=["GET"])
-@login_required(role="admin")
+@login_required
 def tasks_form():
     return render_template("tasks/new.html", form = TaskForm())
 
 @app.route("/tasks/", methods=["POST"])
-@login_required(role="admin")
+@login_required
 def tasks_create():
     form = TaskForm(request.form)
 
@@ -34,12 +34,12 @@ def tasks_create():
     return redirect(url_for("tasks_index"))
 
 @app.route("/tasks/edit/<task_id>/", methods=["GET"])
-@login_required(role="admin")
+@login_required
 def tasks_edit(task_id):
     return render_template("tasks/edit.html", task_id = task_id, form = TaskForm(), task=Task.query.get(task_id), tags=Tag.query.all())
 
 @app.route("/tasks/edit/<task_id>/", methods=["POST"])
-@login_required(role="admin")
+@login_required
 def tasks_editor(task_id):
     form = TaskForm(request.form)
     task = Task.query.get(task_id)
@@ -64,7 +64,7 @@ def tasks_editor(task_id):
     return redirect(url_for("tasks_edit", task_id = task_id))
   
 @app.route("/tasks/<task_id>/", methods=["POST"])
-@login_required(role="admin")
+@login_required
 def tasks_delete(task_id):
     task = Task.query.get(task_id)
 
@@ -74,12 +74,12 @@ def tasks_delete(task_id):
     return redirect(url_for("tasks_index"))
 
 @app.route("/tasks//tasktags/<task_id>", methods=["GET"])
-@login_required(role="admin")
+@login_required
 def tasks_tagsget(task_id):
     return render_template("tasks/tasktags.html", task_id = task_id, task=Task.query.get(task_id), tags=Tag.query.all())
 
 @app.route("/tasks//tasktags/<task_id>", methods=["POST"])
-@login_required(role="admin")
+@login_required
 def tasks_tags(task_id):
     task = Task.query.get(task_id)
     tagtask = request.form.getlist('tags')
