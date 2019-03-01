2dd0aaee092b9144150a6cd7cb56941aecd9aaae
Susanna Rajamäki <strajama@gmail.com>
2019-03-01 16:55:18 +0200

    salasana vaaditaan

--- application/auth/forms.py
+++ application/auth/forms.py
@@ -11,7 +11,7 @@ def validate_first_username(form, field):
 class LoginForm(FlaskForm):
     name = StringField("Name", [validators.Length(min=2, max=20)])
     username = StringField("Username", [validators.Length(min=2, max=20), validate_first_username])
-    password = PasswordField("Password")
+    password = PasswordField("Password", [validators.Length(min=8, max=20)])
     role = SelectField(u'Role', choices=[('admin', 'admin'), ('worker', 'worker')])
   
     class Meta:
